b3cadb0b8d7c9da3e1c72b35ec0cc07f1bf80efa
riktasiddique <riktasiddique17@gmail.com>
2022-03-09 13:34:27 +0600

    second-commit

---
+++ comparison.js
@@ -0,0 +1,10 @@
+var number1 = 50;
+if(number1 === 500){
+    console.log(true);
+}else if(!number1 == 50){
+    console.log(false);
+}else if(number1 === '60'){
+    console.log('===');
+}else if(number1 > 30){
+    console.log('Number is greater than 30');
+}
